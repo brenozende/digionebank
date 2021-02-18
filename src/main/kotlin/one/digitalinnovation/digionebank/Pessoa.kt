a3348f64e7f210ab9ef69661a12d572eb3362f6b
Breno <brenozende@gmail.com>
2021-02-18 19:26:04 -0300

    updated to make person and employee abstract classes

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,19 +1,6 @@
 package one.digitalinnovation.digionebank
 
-class Pessoa {
-    var nome: String = "Breno"
-
-    var cpf: String = "123.123.123-12"
-    private set     //deixando apenas a função set da var cpf como private
-
-    constructor()
-
-    fun pessoaInfo() = "$nome e $cpf"
-}
-
-fun main(){
-    val breno = Pessoa()
-
-    println(breno.pessoaInfo())
-
-}
+abstract class Pessoa(
+    val nome: String,
+    val cpf: String
+)
