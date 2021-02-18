a99d4a4d3d0595cef128a716e52c475181f1a9cb
Breno <brenozende@gmail.com>
2021-02-18 16:25:32 -0300

    created function pessoaInfo

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -4,15 +4,16 @@ class Pessoa {
     var nome: String = "Breno"
 
     var cpf: String = "123.123.123-12"
-    private set     //deixando apenas a propriedade set da var cpf como private
+    private set     //deixando apenas a função set da var cpf como private
 
+    constructor()
 
+    fun pessoaInfo() = "$nome e $cpf"
 }
 
 fun main(){
     val breno = Pessoa()
 
-    println(breno.nome)
-    println(breno.cpf)
+    println(breno.pessoaInfo())
 
 }
